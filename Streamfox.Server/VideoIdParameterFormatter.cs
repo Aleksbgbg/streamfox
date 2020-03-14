e0eb6fb7b2299c2fa509f89937d108fbbcc3d82e
Aleksbgbg <kozichka01@abv.bg>
2020-03-14 22:10:56 +0000

    [VideoIdParameterFormatter] Add IInputFormatter to parse VideoId parameters.

---
+++ Streamfox.Server/VideoIdParameterFormatter.cs
@@ -0,0 +1,28 @@
+﻿namespace Streamfox.Server
+{
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Mvc.Formatters;
+
+    public class VideoIdParameterFormatter : IInputFormatter
+    {
+        public bool CanRead(InputFormatterContext context)
+        {
+            return context.ModelType == typeof(VideoId);
+        }
+
+        public Task<InputFormatterResult> ReadAsync(InputFormatterContext context)
+        {
+            string parameterName = context.Metadata.ParameterName;
+            string parameterValue = (string)context.HttpContext.Request.RouteValues[parameterName];
+
+            if (long.TryParse(parameterValue, out long value))
+            {
+                VideoId videoId = new VideoId(value);
+                return InputFormatterResult.SuccessAsync(videoId);
+            }
+
+            return InputFormatterResult.FailureAsync();
+        }
+    }
+}
