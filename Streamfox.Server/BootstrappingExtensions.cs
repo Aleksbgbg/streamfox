c8f10e4a764602233eb80dd21798998d738e3c7c
Aleksbgbg <kozichka01@abv.bg>
2020-03-14 22:19:48 +0000

    [BootstrappingExtensions] Add AddVideoHosting extension to register implementations for all video hosting interfaces.

---
+++ Streamfox.Server/BootstrappingExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace Streamfox.Server
+{
+    using Microsoft.Extensions.DependencyInjection;
+
+    public static class BootstrappingExtensions
+    {
+        public static IServiceCollection AddVideoHosting(this IServiceCollection services)
+        {
+            services.AddTransient<VideoDirectoryHandler>();
+            services.AddTransient<IFileSystemChecker>(factory => factory.GetService<VideoDirectoryHandler>());
+            services.AddTransient<IFileSystemManipulator>(factory => factory.GetService<VideoDirectoryHandler>());
+            services.AddTransient<IVideoLoader, VideoLoaderFromDisk>();
+            services.AddTransient<VideoRetrievalClerk>();
+            services.AddTransient<IVideoSaver, VideoSaverToDisk>();
+            services.AddTransient<IVideoIdGenerator, SnowflakeVideoIdGenerator>();
+            services.AddTransient<VideoStorageClerk>();
+            services.AddTransient<IVideoClerk, VideoClerkFacade>();
+
+            return services;
+        }
+    }
+}
