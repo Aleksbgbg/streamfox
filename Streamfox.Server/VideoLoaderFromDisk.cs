0829f0c819e8f4f2f07e9fea41c97c7898a34368
Aleksbgbg <kozichka01@abv.bg>
2020-03-14 22:16:20 +0000

    [VideoLoaderFromDisk] Add class to load videos from disk.

---
+++ Streamfox.Server/VideoLoaderFromDisk.cs
@@ -0,0 +1,27 @@
+﻿namespace Streamfox.Server
+{
+    using System.IO;
+
+    public class VideoLoaderFromDisk : IVideoLoader
+    {
+        private readonly IFileSystemChecker _fileSystemChecker;
+
+        private readonly IFileSystemManipulator _fileSystemManipulator;
+
+        public VideoLoaderFromDisk(IFileSystemChecker fileSystemChecker, IFileSystemManipulator fileSystemManipulator)
+        {
+            _fileSystemChecker = fileSystemChecker;
+            _fileSystemManipulator = fileSystemManipulator;
+        }
+
+        public Optional<Stream> LoadVideo(string label)
+        {
+            if (_fileSystemChecker.FileExists(label))
+            {
+                return Optional<Stream>.Of(_fileSystemManipulator.OpenFile(label));
+            }
+
+            return Optional<Stream>.Empty();
+        }
+    }
+}
