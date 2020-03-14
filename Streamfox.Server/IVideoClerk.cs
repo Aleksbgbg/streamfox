bbf2cc4c10cbc4c0b02f9eca23e51e3b651cec09
Aleksbgbg <kozichka01@abv.bg>
2020-03-14 11:48:13 +0000

    [VideoClerkFacade] Add common interface which forwards requests to VideoRetrieval/VideoStorage clerks.

---
+++ Streamfox.Server/IVideoClerk.cs
@@ -0,0 +1,11 @@
+﻿namespace Streamfox.Server
+{
+    using System.IO;
+
+    public interface IVideoClerk
+    {
+        VideoId StoreVideo(Stream videoStream);
+
+        Optional<Stream> RetrieveVideo(VideoId videoId);
+    }
+}
---
+++ Streamfox.Server/VideoClerkFacade.cs
@@ -0,0 +1,27 @@
+﻿namespace Streamfox.Server
+{
+    using System.IO;
+
+    public class VideoClerkFacade : IVideoClerk
+    {
+        private readonly VideoRetrievalClerk _videoRetrievalClerk;
+
+        private readonly VideoStorageClerk _videoStorageClerk;
+
+        public VideoClerkFacade(VideoRetrievalClerk videoRetrievalClerk, VideoStorageClerk videoStorageClerk)
+        {
+            _videoRetrievalClerk = videoRetrievalClerk;
+            _videoStorageClerk = videoStorageClerk;
+        }
+
+        public Optional<Stream> RetrieveVideo(VideoId videoId)
+        {
+            return _videoRetrievalClerk.RetrieveVideo(videoId);
+        }
+
+        public VideoId StoreVideo(Stream videoStream)
+        {
+            return _videoStorageClerk.StoreVideo(videoStream);
+        }
+    }
+}
