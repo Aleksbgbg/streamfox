c33bad53275070150b5a7b8c813b00578d6c0cc1
Aleksbgbg <kozichka01@abv.bg>
2020-03-17 16:32:37 +0000

    [VideoIdParameterFormatterTest] Refactor tests.

--- Streamfox.Server.Tests/Unit/Controllers/Formatters/VideoIdParameterFormatterTest.cs
+++ Streamfox.Server.Tests/Unit/Controllers/Formatters/VideoIdParameterFormatterTest.cs
@@ -1,6 +1,5 @@
 ﻿namespace Streamfox.Server.Tests.Unit.Controllers.Formatters
 {
-    using System.IO;
     using System.Reflection;
     using System.Threading.Tasks;
 
@@ -34,7 +33,9 @@
         {
             const string videoIdString = "123";
 
-            InputFormatterResult result = await _videoIdParameterFormatter.ReadAsync(SetupFormatterContext(videoIdString));
+            InputFormatterResult result =
+                    await _videoIdParameterFormatter.ReadAsync(
+                            SetupFormatterContext(videoIdString));
 
             Assert.IsType<VideoId>(result.Model);
             VideoId videoId = (VideoId)result.Model;
@@ -44,31 +45,35 @@
         [Fact]
         public async Task ReturnsFailedResultForInvalidVideoId()
         {
-            const string videoIdString = "9999999999999999999";
+            string videoIdString = $"9{long.MaxValue}";
 
-            InputFormatterResult result = await _videoIdParameterFormatter.ReadAsync(SetupFormatterContext(videoIdString));
+            InputFormatterResult result =
+                    await _videoIdParameterFormatter.ReadAsync(
+                            SetupFormatterContext(videoIdString));
 
             Assert.True(result.HasError);
         }
 
-        private static InputFormatterContext SetupFormatterContext(string value = "")
+        private static InputFormatterContext SetupFormatterContext(string videoIdString = "")
         {
             HttpContext httpContext = new DefaultHttpContext();
             httpContext.Request.RouteValues = new RouteValueDictionary
             {
-                { "videoId", value }
+                { "videoId", videoIdString }
             };
 
             ParameterInfo videoIdParam = typeof(VideoIdParameterFormatterTest)
-                                         .GetMethod(nameof(DummyMethod), BindingFlags.NonPublic | BindingFlags.Static)
+                                         .GetMethod(
+                                                 nameof(DummyMethod),
+                                                 BindingFlags.NonPublic | BindingFlags.Static)
                                          .GetParameters()[0];
-            ModelMetadata modelMetadata = new EmptyModelMetadataProvider().GetMetadataForParameter(videoIdParam, typeof(VideoId));
 
-            return new InputFormatterContext(httpContext,
-                                             modelName: string.Empty,
-                                             modelState: new ModelStateDictionary(),
-                                             modelMetadata,
-                                             readerFactory: (_, __) => TextReader.Null);
+            ModelMetadata modelMetadata =
+                    new EmptyModelMetadataProvider().GetMetadataForParameter(
+                            videoIdParam,
+                            typeof(VideoId));
+
+            return TestUtils.InputFormatterContextFor(httpContext, modelMetadata);
         }
 
         private static void DummyMethod(VideoId videoId)
