a72882cc462fd2ab0207c805571028eaf47d5567
Aleksbgbg <kozichka01@abv.bg>
2020-03-17 17:24:29 +0000

    [VideoStorageClerkTest] Refactor tests.

--- Streamfox.Server.Tests/Unit/VideoManagement/VideoStorageClerkTest.cs
+++ Streamfox.Server.Tests/Unit/VideoManagement/VideoStorageClerkTest.cs
@@ -21,37 +21,40 @@
         {
             _videoIdGenerator = new Mock<IVideoIdGenerator>();
             _videoSaverMock = new Mock<IVideoSaver>();
-            _videoStorageClerk = new VideoStorageClerk(_videoIdGenerator.Object, _videoSaverMock.Object);
+            _videoStorageClerk = new VideoStorageClerk(
+                    _videoIdGenerator.Object,
+                    _videoSaverMock.Object);
         }
 
-        [Fact]
-        public async Task GeneratesANewLabelForEachVideo()
+        [Theory]
+        [InlineData(123)]
+        [InlineData(456)]
+        public async Task GeneratesANewLabelForVideo(long videoIdValue)
         {
-            _videoIdGenerator.SetupSequence(generator => generator.GenerateVideoId())
-                             .Returns(new VideoId(534))
-                             .Returns(new VideoId(780));
+            SetupVideoIdOnGeneration(videoIdValue);
 
-            VideoId videoId1 = await _videoStorageClerk.StoreVideo(TestUtils.MockStream());
-            VideoId videoId2 = await _videoStorageClerk.StoreVideo(TestUtils.MockStream());
+            VideoId videoId = await _videoStorageClerk.StoreVideo(TestUtils.MockStream());
 
-            Assert.Equal(534, videoId1.Value);
-            Assert.Equal(780, videoId2.Value);
+            Assert.Equal(videoIdValue, videoId.Value);
         }
 
-        [Fact]
-        public async Task SavesEachVideoWithTheCorrectLabel()
+        [Theory]
+        [InlineData(123)]
+        [InlineData(456)]
+        public async Task SavesVideoUsingIdAsLabel(long videoIdValue)
         {
-            Stream videoStream1 = TestUtils.MockStream();
-            Stream videoStream2 = TestUtils.MockStream();
-            _videoIdGenerator.SetupSequence(generator => generator.GenerateVideoId())
-                             .Returns(new VideoId(123))
-                             .Returns(new VideoId(456));
+            Stream videoStream = TestUtils.MockStream();
+            SetupVideoIdOnGeneration(videoIdValue);
 
-            await _videoStorageClerk.StoreVideo(videoStream1);
-            await _videoStorageClerk.StoreVideo(videoStream2);
+            await _videoStorageClerk.StoreVideo(videoStream);
 
-            _videoSaverMock.Verify(saver => saver.SaveVideo("123", videoStream1));
-            _videoSaverMock.Verify(saver => saver.SaveVideo("456", videoStream2));
+            _videoSaverMock.Verify(saver => saver.SaveVideo(videoIdValue.ToString(), videoStream));
+        }
+
+        private void SetupVideoIdOnGeneration(long videoIdValue)
+        {
+            _videoIdGenerator.Setup(generator => generator.GenerateVideoId())
+                             .Returns(new VideoId(videoIdValue));
         }
     }
 }
