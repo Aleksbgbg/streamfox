0341ce17b67d9f4fcb5129ddc863d138b8820602
Aleksbgbg <kozichka01@abv.bg>
2020-08-22 11:34:26 +0100

    [ProcessExtensions] Remove timeout functionality as no longer needed.

--- Streamfox.Server/Processing/ProcessExtensions.cs
+++ Streamfox.Server/Processing/ProcessExtensions.cs
@@ -1,29 +1,13 @@
 ﻿namespace Streamfox.Server.Processing
 {
     using System.Diagnostics;
-    using System.Threading;
     using System.Threading.Tasks;
 
     public static class ProcessExtensions
     {
-        public static Task<int> StartAsync(this Process process, int timeout = -1)
+        public static Task<int> StartAsync(this Process process)
         {
             TaskCompletionSource<int> taskCompletionSource = new TaskCompletionSource<int>();
-
-            if (timeout > 0)
-            {
-                CancellationTokenSource cancellationTokenSource =
-                        new CancellationTokenSource(timeout);
-                cancellationTokenSource.Token.Register(
-                        () =>
-                        {
-                            if (!taskCompletionSource.Task.IsCompleted)
-                            {
-                                taskCompletionSource.SetResult(-1);
-                                process.Dispose();
-                            }
-                        });
-            }
 
             process.EnableRaisingEvents = true;
             process.Exited += (sender, e) =>
