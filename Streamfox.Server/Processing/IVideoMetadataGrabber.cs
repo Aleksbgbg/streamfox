04979b99ffa7e2b54fb8f3639c3698fd5b88d9ff
Aleksbgbg <kozichka01@abv.bg>
2020-10-02 18:02:50 +0100

    Implement metadata extractor.

---
+++ Streamfox.Server/Processing/IVideoMetadataGrabber.cs
@@ -0,0 +1,9 @@
+﻿namespace Streamfox.Server.Processing
+{
+    using System.Threading.Tasks;
+
+    public interface IVideoMetadataGrabber
+    {
+        Task<VideoMetadata> GrabMetadata(string videoPath);
+    }
+}
