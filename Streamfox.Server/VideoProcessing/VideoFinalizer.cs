6f5d83bb19fc6bde2e80ba77f6ec3bafc011d0b7
Aleksbgbg <kozichka01@abv.bg>
2020-10-03 10:49:59 +0100

    Implement video finalizer.

---
+++ Streamfox.Server/VideoProcessing/VideoFinalizer.cs
@@ -0,0 +1,21 @@
+﻿namespace Streamfox.Server.VideoProcessing
+{
+    using System.Threading.Tasks;
+
+    using Streamfox.Server.VideoManagement;
+
+    public class VideoFinalizer : IVideoFinalizer
+    {
+        private readonly IIntermediateVideoDeleter _intermediateVideoDeleter;
+
+        public VideoFinalizer(IIntermediateVideoDeleter intermediateVideoDeleter)
+        {
+            _intermediateVideoDeleter = intermediateVideoDeleter;
+        }
+
+        public Task FinalizeVideoProcessing(VideoId videoId)
+        {
+            return _intermediateVideoDeleter.DeleteIntermediateVideo(videoId);
+        }
+    }
+}
