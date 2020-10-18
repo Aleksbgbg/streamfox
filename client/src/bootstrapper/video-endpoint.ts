5e1a3b1dd0f3c8fc050a5797b25bf4744f135644
Aleksbgbg <kozichka01@abv.bg>
2020-10-18 12:42:08 +0100

    Fix bug where URLs would mismatch between the server and client in production.

--- client/src/bootstrapper/video-endpoint.ts
+++ client/src/bootstrapper/video-endpoint.ts
@@ -1,8 +1,13 @@
+import { endpointResolver } from "@/bootstrapper/endpoint-resolver";
+import { CommunicatorFactory } from "@/endpoints/communicator-factory";
 import { VideoEndpoint } from "@/endpoints/video-endpoint";
 import { VideoLister } from "@/endpoints/video-lister";
+import { VideoProgressFetcher } from "@/endpoints/video-progress-fetcher";
 import { VideoUploader } from "@/endpoints/video-uploader";
 
-const videoEndpoint = new VideoEndpoint();
+const communicatorFactory = new CommunicatorFactory(endpointResolver);
+const videoEndpoint = new VideoEndpoint(communicatorFactory.createCommunicator("videos"));
 
 export const videoLister: VideoLister = videoEndpoint;
 export const videoUploader: VideoUploader = videoEndpoint;
+export const videoProgressFetcher: VideoProgressFetcher = videoEndpoint;
