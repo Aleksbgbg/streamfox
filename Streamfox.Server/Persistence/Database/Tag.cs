2d75837609137940f637ed04367d13ee0a02931c
Aleksbgbg <kozichka01@abv.bg>
2020-11-08 13:20:39 +0000

    Add database context.

---
+++ Streamfox.Server/Persistence/Database/Tag.cs
@@ -0,0 +1,16 @@
+﻿namespace Streamfox.Server.Persistence.Database
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    public class Tag
+    {
+        [Key]
+        public long Id { get; set; }
+
+        [Required]
+        public string Value { get; set; }
+
+        public ICollection<VideoTag> VideoTags { get; set; }
+    }
+}
---
+++ Streamfox.Server/Persistence/Database/VideoTag.cs
@@ -0,0 +1,17 @@
+﻿namespace Streamfox.Server.Persistence.Database
+{
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    public class VideoTag
+    {
+        [ForeignKey(nameof(Video))]
+        public long VideoId { get; set; }
+
+        public Video Video { get; set; }
+
+        [ForeignKey(nameof(Tag))]
+        public long TagId { get; set; }
+
+        public Tag Tag { get; set; }
+    }
+}
