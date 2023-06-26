2f476b3c5fa5c67e11fb49d13c4faa960b466d73
Aleksbgbg <aleks.todorov.1337@gmail.com>
2023-06-26 19:48:29 +0100

    Move styles to src dir.

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from "vue";
 import "video.js";
 import "video.js/dist/video-js.css";
 import App from "@/App.vue";
-import "@/assets/main.css";
 import { router } from "@/router";
+import "@/style.css";
 
 createApp(App).use(router).mount("#app");
