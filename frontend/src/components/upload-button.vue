86889abaeea74f57ea95667bd1bbb0a20a526b74
Aleksbgbg <aleks.todorov.1337@gmail.com>
2023-07-02 19:01:16 +0100

    Fix separator in upload button to restore animation.

--- frontend/src/components/upload-button.vue
+++ frontend/src/components/upload-button.vue
@@ -1,4 +1,4 @@
 <template lang="pug">
 router-link(:to="{ name: 'upload' }")
-  button.rounded.font-bold.border-b-4.bg-blue-500.border-blue-700.hover-bg-blue-700.hover-border-blue-900.transition.duration-500.ease-in-out.transform.hover--translate-y-1.hover-scale-110.mx-5.py-2.px-4 Upload Video
+  button.rounded.font-bold.border-b-4.bg-blue-500.border-blue-700.hover_bg-blue-700.hover_border-blue-900.transition.duration-500.ease-in-out.transform.hover_-translate-y-1.hover_scale-110.mx-5.py-2.px-4 Upload Video
 </template>
