4f63096aa8e0f56cb5c41fc8cd135ec88c2b7c17
Aleksbgbg <aleks.todorov.1337@gmail.com>
2023-07-03 19:46:40 +0100

    Convert to Open Sans font.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -12,7 +12,7 @@ import VNavbar from "@/views/navbar.vue";
 
 <style lang="stylus">
 #app
-  font-family: Avenir, Helvetica, Arial, sans-serif
+  font-family: "Open Sans", sans-serif
   -webkit-font-smoothing: antialiased
   -moz-osx-font-smoothing: grayscale
 </style>
