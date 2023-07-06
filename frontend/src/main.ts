1d33ab13a188e20be493e972fd4cf731f34177ce
Aleksbgbg <aleks.todorov.1337@gmail.com>
2023-07-06 19:20:18 +0100

    Add pinia.

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,9 +4,10 @@ import "video.js";
 import "video.js/dist/video-js.css";
 import App from "@/App.vue";
 import { router } from "@/router";
+import { pinia } from "@/store";
 import "@/style.css";
 import { refreshLoginStatus } from "@/utils/auth";
 
 refreshLoginStatus();
 
-createApp(App).use(router).mount("#app");
+createApp(App).use(router).use(pinia).mount("#app");
