2d1944bf329d2c59e3f49c0691584840e6263297
Aleksbgbg <aleks.todorov.1337@gmail.com>
2023-07-06 20:29:17 +0100

    Add login endpoint.

--- frontend/src/endpoints/auth.ts
+++ frontend/src/endpoints/auth.ts
@@ -1,5 +1,7 @@
 import axios, { AxiosResponse } from "axios";
 
+export type Authorization = AxiosResponse<{ token: string }>;
+
 export interface Registration {
   username: string;
   emailAddress: string;
@@ -7,6 +9,15 @@ export interface Registration {
   repeatPassword: string;
 }
 
-export function register(data: Registration): Promise<AxiosResponse<{ token: string }>> {
+export function register(data: Registration): Promise<Authorization> {
   return axios.post("/api/auth/register", data);
+}
+
+export interface Credentials {
+  username: string;
+  password: string;
+}
+
+export function requestLogin(data: Credentials): Promise<Authorization> {
+  return axios.post("/api/auth/login", data);
 }
