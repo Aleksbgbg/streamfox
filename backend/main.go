d8a0283c5930d08ea480a16360ecae72bc3086ec
Aleksbgbg <aleks.todorov.1337@gmail.com>
2023-07-09 19:22:35 +0100

    Add GET video thumbnail endpoint.

--- backend/main.go
+++ backend/main.go
@@ -23,6 +23,7 @@ func main() {
 	api.GET("/user", controllers.GetUser)
 
 	api.POST("/videos", controllers.CreateVideo)
+	api.GET("/videos/:id/thumbnail", controllers.GetVideoThumbnail)
 	api.GET("/videos/:id/stream", controllers.GetVideoStream)
 	api.PUT("/videos/:id/settings", controllers.UpdateVideo)
 	api.PUT("/videos/:id/stream", controllers.UploadVideo)
