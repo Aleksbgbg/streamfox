fc1b16a268a21729bb6f4d97d6112ba800171bd4
Aleksbgbg <aleks.todorov.1337@gmail.com>
2023-07-14 18:51:44 +0100

    Switch upload button to new colour scheme.

--- frontend/src/components/upload-button.vue
+++ frontend/src/components/upload-button.vue
@@ -1,4 +1,4 @@
 <template lang="pug">
 router-link(:to="{ name: 'upload' }")
-  button(class="bg-blue-500 border-blue-700 hover:bg-blue-700 hover:border-blue-900 hover:-translate-y-1 hover:scale-110 transform-gpu font-bold rounded border-b-4 transition duration-500 ease-in-out px-4 py-2 mx-5") Upload Video
+  button(class="bg-frost-blue border-snow-light hover:bg-frost-deep hover:border-snow-dark hover:-translate-y-1 hover:scale-110 transform-gpu font-bold rounded border-b-4 transition duration-500 ease-in-out px-4 py-2 mx-5") Upload Video
 </template>
