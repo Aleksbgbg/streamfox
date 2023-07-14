693b1e56dab817777b412954c5cc0b17bbf5cf6f
Aleksbgbg <aleks.todorov.1337@gmail.com>
2023-07-14 22:32:56 +0100

    Add textarea field, a copy of the input field.

---
+++ frontend/src/components/forms/textarea.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { computed } from "vue";
+import { asInputElement } from "@/utils/cast";
+import { toLowerCamelCase } from "@/utils/strings";
+
+defineEmits<{
+  (e: "update:modelValue", value: string): void;
+}>();
+
+interface Props {
+  title: string;
+  placeholder?: string;
+  modelValue: string;
+  errors: string[];
+}
+
+const props = defineProps<Props>();
+
+const label = computed(() => toLowerCamelCase(props.title));
+</script>
+
+<template lang="pug">
+label.justify-self-end.mt-2(:for="label")
+  span {{ title }}
+  span.text-aurora-red *
+.flex.flex-col
+  textarea(
+    class="bg-polar-darkest text-white placeholder-snow-dark placeholder:italic focus:border-aurora-green focus:outline-none resize-none leading-normal rounded border py-2 px-3"
+    :class="[errors.length === 0 ? 'border-polar-lightest' : 'border-aurora-red']"
+    :id="label" :label="label" :placeholder="placeholder"
+    :value="modelValue"
+    @input="$emit('update:modelValue', asInputElement($event.target).value)"
+  )
+  p.text-aurora-red(v-for="error of errors") {{ error }}
+</template>
