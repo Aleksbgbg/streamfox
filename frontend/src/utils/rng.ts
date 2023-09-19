6004b930188cd152792e5434df78e0d987c8b169
Aleks Todorov <aleks.todorov.1337@gmail.com>
2023-09-19 20:38:44 +0100

    Add a random number generator util

---
+++ frontend/src/utils/rng.ts
@@ -0,0 +1,48 @@
+function cyrb128(str: string): [number, number, number, number] {
+  let h1 = 1779033703;
+  let h2 = 3144134277;
+  let h3 = 1013904242;
+  let h4 = 2773480762;
+  for (let i = 0, k; i < str.length; i++) {
+    k = str.charCodeAt(i);
+    h1 = h2 ^ Math.imul(h1 ^ k, 597399067);
+    h2 = h3 ^ Math.imul(h2 ^ k, 2869860233);
+    h3 = h4 ^ Math.imul(h3 ^ k, 951274213);
+    h4 = h1 ^ Math.imul(h4 ^ k, 2716044179);
+  }
+  h1 = Math.imul(h3 ^ (h1 >>> 18), 597399067);
+  h2 = Math.imul(h4 ^ (h2 >>> 22), 2869860233);
+  h3 = Math.imul(h1 ^ (h3 >>> 17), 951274213);
+  h4 = Math.imul(h2 ^ (h4 >>> 19), 2716044179);
+  h1 ^= h2 ^ h3 ^ h4;
+  h2 ^= h1;
+  h3 ^= h1;
+  h4 ^= h1;
+  return [h1 >>> 0, h2 >>> 0, h3 >>> 0, h4 >>> 0];
+}
+
+export type Rng = (min: number, max: number) => number;
+
+function sfc32(a: number, b: number, c: number, d: number): Rng {
+  return function (min: number, max: number): number {
+    a >>>= 0;
+    b >>>= 0;
+    c >>>= 0;
+    d >>>= 0;
+    let t = (a + b) | 0;
+    a = b ^ (b >>> 9);
+    b = (c + (c << 3)) | 0;
+    c = (c << 21) | (c >>> 11);
+    d = (d + 1) | 0;
+    t = (t + d) | 0;
+    c = (c + t) | 0;
+
+    const result = (t >>> 0) / 4294967296;
+    return min + result * (max - min);
+  };
+}
+
+export function newRng(seed: string): Rng {
+  const seedState = cyrb128(seed);
+  return sfc32(seedState[0], seedState[1], seedState[2], seedState[3]);
+}
