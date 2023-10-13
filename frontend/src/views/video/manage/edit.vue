bf9d0a8118cdf684ba24f59df7fbc9724c1bb020
Aleks Todorov <aleks.todorov.1337@gmail.com>
2023-10-13 18:40:14 +0100

    Put the edit details view inside a tab
    We make this change in preparation of adding a subtitles tab.

--- frontend/src/views/video/manage/edit.vue
+++ frontend/src/views/video/manage/edit.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
 import CFormLayout from "@/components/layout/form.vue";
+import CTab from "@/components/tab-control/tab.vue";
+import CTabs from "@/components/tab-control/tabs.vue";
 import { type VideoId } from "@/endpoints/video";
 import CEditDetails from "@/views/video/manage/details.vue";
 
@@ -10,5 +12,7 @@ defineProps<{
 
 <template lang="pug">
 c-form-layout(title="Edit Video")
-  c-edit-details(:video-id="videoId")
+  c-tabs
+    c-tab(title="Details")
+      c-edit-details(:video-id="videoId")
 </template>
