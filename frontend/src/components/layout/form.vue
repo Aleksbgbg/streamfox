0197b9e2def3be47ffa1cea05eae4bfebf7389dc
Aleks Todorov <aleks.todorov.1337@gmail.com>
2023-10-24 22:06:19 +0100

    Adjust form layout to work well on small screens
    On small screens we now display the content full screen rather than
    having a bordered, centered cutout.
    
    In addition, the content won't overflow below the height of the screen.

--- frontend/src/components/layout/form.vue
+++ frontend/src/components/layout/form.vue
@@ -5,8 +5,10 @@ defineProps<{
 </script>
 
 <template lang="pug">
-.flex.justify-center.mt-5
-  .bg-polar-darkest.border.border-frost-deep.rounded-t.overflow-hidden(class="w-2/3")
+.flex.h-full.max-h-full(class="md:justify-center md:h-auto md:py-5")
+  .flex.flex-col.bg-polar-light.border-frost-deep.w-full.max-h-full.overflow-hidden(
+    class="md:border md:rounded-t md:w-4/5"
+  )
     h2.bg-polar-dark.border-b.border-frost-deep.text-center.py-2 {{ title }}
     slot
 </template>
