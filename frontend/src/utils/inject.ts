82b3fd3f40c3eaae606ea16011e1f2967666cc40
Aleks Todorov <aleks.todorov.1337@gmail.com>
2023-10-26 19:42:22 +0100

    Add injectStrict util
    Panics when the injection key is not available as such errors should not
    occur.

---
+++ frontend/src/utils/inject.ts
@@ -0,0 +1,12 @@
+import { type InjectionKey, inject } from "vue";
+import { panic } from "@/utils/panic";
+
+export function injectStrict<T>(key: InjectionKey<T>): T {
+  const result = inject(key);
+
+  if (typeof result === undefined) {
+    panic(`no provider for injection key ${key.toString()}`);
+  }
+
+  return result as T;
+}
