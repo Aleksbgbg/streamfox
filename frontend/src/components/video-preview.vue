263b116a1cf8483a89e146f8aaf1a6e020135727
Aleks Todorov <aleks.todorov.1337@gmail.com>
2023-11-26 16:30:28 +0000

    Remove explicit props prefix in video preview template

--- frontend/src/components/video-preview.vue
+++ frontend/src/components/video-preview.vue
@@ -3,25 +3,25 @@ import CWatchLink from "@/components/watch-link.vue";
 import { type VideoInfo, videoThumbnail } from "@/endpoints/video";
 import { dateToElapsedTimeString, secsToDurationString } from "@/utils/strings";
 
-const props = defineProps<{
+defineProps<{
   video: VideoInfo;
 }>();
 </script>
 
 <template lang="pug">
-c-watch-link.group.w-full(:videoId="props.video.id")
+c-watch-link.group.w-full(:videoId="video.id")
   .relative.aspect-video
     img(src="@/assets/fox.png" alt="")
-    img.absolute.top-0(:src="videoThumbnail(props.video.id)" alt="")
+    img.absolute.top-0(:src="videoThumbnail(video.id)" alt="")
     span(
       class="absolute bottom-0.5 right-0.5 text-sm opacity-85 bg-black px-1"
-    ) {{ secsToDurationString(props.video.durationSecs) }}
+    ) {{ secsToDurationString(video.durationSecs) }}
   h3.break-words.line-clamp-2.font-semibold.mt-2(
     class="group-hover:underline group-hover:text-aurora-yellow"
-  ) {{ props.video.name }}
-  p.text-sm {{ props.video.creator.username }}
+  ) {{ video.name }}
+  p.text-sm {{ video.creator.username }}
   p.text-sm
-    | {{ props.video.views }} {{ props.video.views === 1 ? 'view' : 'views' }}
+    | {{ video.views }} {{ video.views === 1 ? 'view' : 'views' }}
     | •
-    | {{ dateToElapsedTimeString(props.video.uploadedAt) }}
+    | {{ dateToElapsedTimeString(video.uploadedAt) }}
 </template>
