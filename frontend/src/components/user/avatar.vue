b07f64432a4b117a384a3ce7b93dad2d4594ef8d
Aleks Todorov <aleks.todorov.1337@gmail.com>
2024-01-01 19:11:00 +0000

    Make avatar colour reactive to props changes

--- frontend/src/components/user/avatar.vue
+++ frontend/src/components/user/avatar.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { computed } from "vue";
 import type { User } from "@/endpoints/user";
 import { newRng } from "@/utils/rng";
 
@@ -7,16 +8,20 @@ const props = defineProps<{
   size: string;
 }>();
 
-const rng = newRng(props.user.id);
+const colour = computed(() => {
+  const rng = newRng(props.user.id);
 
-const h = Math.round(rng(0, 360));
-const s = Math.round(rng(25, 60));
-const l = Math.round(rng(25, 50));
+  const h = Math.round(rng(0, 360));
+  const s = Math.round(rng(25, 60));
+  const l = Math.round(rng(25, 50));
+
+  return { h, s, l };
+});
 </script>
 
 <template lang="pug">
 .flex.items-center.justify-center.rounded-full.aspect-square.select-none(
-  :style="{ 'background-color': `hsl(${h}, ${s}%, ${l}%)`, 'width': size, 'height': size, 'container-type': 'inline-size' }"
+  :style="{ 'background-color': `hsl(${colour.h}, ${colour.s}%, ${colour.l}%)`, 'width': size, 'height': size, 'container-type': 'inline-size' }"
 )
   span.font-bold.uppercase(:style="{ 'font-size': '60cqw' }") {{ user.username.charAt(0) }}
 </template>
