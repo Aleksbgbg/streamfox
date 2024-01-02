726a4d179ea7d7997dc4313ab6754ae36d78cb38
Aleks Todorov <aleks.todorov.1337@gmail.com>
2024-01-02 20:36:37 +0000

    Allow dropdown items to have covering anchors
    Wrap dropdown items in c-anchor-root to allow children to use
    c-anchor-cover to cover them completely.

--- frontend/src/components/dropdown/item.vue
+++ frontend/src/components/dropdown/item.vue
@@ -1,6 +1,11 @@
+<script setup lang="ts">
+import CAnchorRoot from "@/components/anchor/root.vue";
+</script>
+
 <template lang="pug">
-li.bg-aurora-purple.truncate.max-w-full.px-3.py-2.cursor-pointer(
-  class="hover:bg-aurora-purple-400"
-)
-  slot
+c-anchor-root
+  li.bg-aurora-purple.truncate.max-w-full.px-3.py-2.cursor-pointer(
+    class="hover:bg-aurora-purple-400"
+  )
+    slot
 </template>
