149dffab96fcadd15a8bad4124d3b5e32fea51df
Aleks Todorov <aleks.todorov.1337@gmail.com>
2024-01-06 17:16:09 +0000

    Try background from Haikei (https://app.haikei.app)

--- frontend/src/app.vue
+++ frontend/src/app.vue
@@ -1,13 +1,15 @@
 <script setup lang="ts">
+import CBackgroundImage from "@/assets/low-poly-grid-haikei.svg";
 import CToastArea from "@/components/toasts/toast-area.vue";
 import CNavbar from "@/views/navbar/navbar.vue";
 </script>
 
 <template lang="pug">
 #app.flex.flex-col.bg-polar-dark.text-white.overflow-hidden.h-screen
-  header.bg-polar-darkest
+  header.bg-polar-darkest(:style="{ 'z-index': 1 }")
     c-navbar
   main.grow.overflow-auto
+    c-background-image.fixed.inset-0(:style="{ 'z-index': 0 }")
     router-view
   c-toast-area
 </template>
