1aed038d73fc51b8294bef3d8e9bf58d8c11ced2
Aleks Todorov <aleks.todorov.1337@gmail.com>
2024-01-18 21:22:07 +0000

    Switch the dropdown theme from purple to blue
    Blue matches the select dropdown better, and the navbar user button can
    be switched to blue to match.

--- frontend/src/components/dropdown/item.vue
+++ frontend/src/components/dropdown/item.vue
@@ -4,8 +4,8 @@ import CAnchorRoot from "@/components/anchor/root.vue";
 
 <template lang="pug">
 c-anchor-root
-  li.bg-aurora-purple.truncate.max-w-full.px-3.py-2.cursor-pointer(
-    class="hover:bg-aurora-purple-400"
+  li.bg-frost-blue.truncate.max-w-full.px-3.py-2.cursor-pointer(
+    class="hover:bg-frost-deep"
   )
     slot
 </template>
