87178c67c259ebf60f15ca31c8fb3120c3a2fd51
Aleks Todorov <aleks.todorov.1337@gmail.com>
2024-02-03 19:39:16 +0000

    Add Axum 'Hello, world!' example

--- backend-rs/src/main.rs
+++ backend-rs/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-  println!("Hello, world!");
+use axum::{routing, Router};
+use std::net::SocketAddr;
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() {
+  let app = Router::new().route("/api/hello-world", routing::get(hello_world));
+
+  let listener = TcpListener::bind(SocketAddr::from(([0, 0, 0, 0], 8601)))
+    .await
+    .expect("could not bind");
+
+  axum::serve(listener, app).await.expect("could not serve");
+}
+
+async fn hello_world() -> &'static str {
+  "Hello, world!"
 }
