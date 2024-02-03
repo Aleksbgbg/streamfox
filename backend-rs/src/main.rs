c807b3beec0ee7a1bea708e461e30651bb40dfc5
Aleks Todorov <aleks.todorov.1337@gmail.com>
2024-02-03 19:47:33 +0000

    Add tracing and logging

--- backend-rs/src/main.rs
+++ backend-rs/src/main.rs
@@ -1,14 +1,34 @@
 use axum::{routing, Router};
 use std::net::SocketAddr;
 use tokio::net::TcpListener;
+use tower_http::trace::{DefaultMakeSpan, DefaultOnResponse, TraceLayer};
+use tracing::{info, Level};
 
 #[tokio::main]
 async fn main() {
-  let app = Router::new().route("/api/hello-world", routing::get(hello_world));
+  tracing_subscriber::fmt()
+    .with_target(false)
+    .compact()
+    .init();
+
+  let app = Router::new()
+    .route("/api/hello-world", routing::get(hello_world))
+    .layer(
+      TraceLayer::new_for_http()
+        .make_span_with(DefaultMakeSpan::new().level(Level::INFO))
+        .on_response(DefaultOnResponse::new().level(Level::INFO)),
+    );
 
   let listener = TcpListener::bind(SocketAddr::from(([0, 0, 0, 0], 8601)))
     .await
     .expect("could not bind");
+
+  info!(
+    "backend available at: {}",
+    listener
+      .local_addr()
+      .expect("could not get listener address")
+  );
 
   axum::serve(listener, app).await.expect("could not serve");
 }
